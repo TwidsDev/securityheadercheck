7fedc94983068638fb8acd073d10c7127a7b8219
TwidsDev <41450399+TwidsDev@users.noreply.github.com>
2019-10-22 11:57:44 +0100

    Simplified code

--- checkheaders.py
+++ checkheaders.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python
 
 import requests,sys
+requestHeaders = ["Strict-Transport-Security","X-Xss-Protection","X-Content-Type-Options","X-Frame-Options","Content-Security-Policy","Public-Key-Pins"]
 
 for url in open("sites.txt"):
-    try:
-        r = requests.head(url.rstrip())
-	print "------------------------------------------"
-	print "Site Headers Being Checked:", url
-        if 'Strict-Transport-Security' in r.headers:
-                print "Strict-Transport-Security:", r.headers['Strict-Transport-Security']
-        else:
-                print "Strict-Transport-Security Not Present"
-	if 'X-Xss-Protection' in r.headers:
-		print "X-Xss-Protection:", r.headers['X-Xss-Protection']
-	else: 
-		print "X-Xss-Protection Not Present"
-        if 'X-Content-Type-Options' in r.headers:
-                print "X-Content-Type-Options:", r.headers['X-Content-Type-Options']
-        else: 
-                print "X-Content-Type-Options Not Present"
-	if 'X-Frame-Options' in r.headers:
-		print "X-Frame-Options:", r.headers['X-Frame-Options']
-	else:
-		print "X-Frame-Options Not Present"
-        if 'Content-Security-Policy' in r.headers:
-                print "Content-Security-Policy:", r.headers['Content-Security-Policy']
-        else: 
-                print "Content-Security-Policy Not Present"
-	if 'Public-Key-Pins' in r.headers:
-		print "Public-Key-Pins:", r.headers['Public-Key-Pins']
-	else:
-		print "Public-Key-Pins Not Present"
-	print "------------------------------------------","\n"
-  
-    except:
-        print "Unknown Exception:",sys.exc_info()[0]
-	raise
+	try:
+		print ("Site Headers Being Checked:", url)
+		r = requests.head(url.rstrip())
+		for i in requestHeaders:
+			if i in r.headers:
+				print(i, r.headers[i])
+			else:
+				print(i + " Not Present")
+	except:
+		print("Unknown Exception:" + sys.exc_info()[0])
